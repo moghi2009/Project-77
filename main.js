80fb2d9dfe65341d4a261df0919f5056063c8b9b
moghi2009 <84376170+moghi2009@users.noreply.github.com>
2021-12-29 11:21:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var first_name = "Amogh";
+var last_name = "Chidrawar";
+var firstLast_name = first_name.concat(last_name);
+console.log(firstLast_name);
+
+var first_number = 75;
+var second_number = 5;
+
+var squareroot = Math.sqrt(first_number);
+console.log(squareroot);
+
+var max = Math.max(first_number, second_number);
+console.log(max);
+
+var min = Math.min(first_number, second_number);
+console.log(min);
